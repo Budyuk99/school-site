46cb90d5e3bb205b7d5cea913e6fc064d76256e2
Nikita Budyuk <57536202+Budyuk99@users.noreply.github.com>
2023-02-05 18:29:04 +0300

    Added changes

--- index.php
+++ index.php
@@ -4,13 +4,48 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css">
+
     <title>Ashot School</title>
 </head>
 <body>
     <header class="header">
-        <div class="header__menu"></div>
+        <div class="header__menu">
+            <menu class="header__menu header__menu_flex-container_color" type="context">
+                <menuitem class="header__menuitem header__menuitem_flex-item"><a href="/"><img src="img/logo.png" alt=""></a></menuitem>
+                <menuitem class="header__menuitem header__menuitem_flex-item"><a href="#">НАРУШЕНИЯ РЕЧИ</a></menuitem>
+                <menuitem class="header__menuitem header__menuitem_flex-item"><a href="#">ЗАДЕРЖКА И НАРУШЕНИЕ РАЗВИТИЯ</a></menuitem>
+                <menuitem class="header__menuitem header__menuitem_flex-item"><a href="#">ПОБЛЕМЫ ОБУЧЕНИЯ И ПОВЕДЕНИЯ</a></menuitem>
+                <menuitem class="header__menuitem header__menuitem_flex-item"><a href="#">НАША КОМАНДА</a></menuitem>
+            </menu>
+        </div>
     </header>
-    <section class=""></section>
+    <section class="slider">
+        <!-- Slider main container -->
+        <div class="swiper">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            <div class="swiper-slide"><img src="img/neyrokorrekciya.jpg" alt="neyrokorrekciya"></div>
+            <div class="swiper-slide"><img src="img/photo.jpg" alt="photo"></div>
+            <div class="swiper-slide"><img src="img/photo_2022-10-20_15-.jpg" alt="photo"></div>
+        </div>
+        <!-- If we need pagination -->
+        <div class="swiper-pagination"></div>
+
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+
+        <!-- If we need scrollbar -->
+        <div class="swiper-scrollbar"></div>
+        </div>
+    </section>
     <footer></footer>
+    
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+    <script src="js/script.js"></script>
 </body>
 </html>
