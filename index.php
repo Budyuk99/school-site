4ca1ea0c123693b416957d7adffffd80a0b1751b
Nikita Budyuk <57536202+Budyuk99@users.noreply.github.com>
2023-02-07 01:37:56 +0300

    Added changes

--- index.php
+++ index.php
@@ -15,13 +15,74 @@
         <div class="header__menu header__menu_height-inherit">
             <menu class="header__menu header__menu_flex-container" type="context">
                 <menuitem class="header__menuitem header__menuitem_flex-item"><a href="/"><img src="img/logo.png" alt=""></a></menuitem>
-                <menuitem class="header__menuitem header__menuitem_flex-item"><a href="#">НАРУШЕНИЯ РЕЧИ</a></menuitem>
-                <menuitem class="header__menuitem header__menuitem_flex-item"><a href="#">ЗАДЕРЖКА И НАРУШЕНИЕ РАЗВИТИЯ</a></menuitem>
-                <menuitem class="header__menuitem header__menuitem_flex-item"><a href="#">ПОБЛЕМЫ ОБУЧЕНИЯ И ПОВЕДЕНИЯ</a></menuitem>
-                <menuitem class="header__menuitem header__menuitem_flex-item"><a href="#">НАША КОМАНДА</a></menuitem>
+                <menuitem class="header__menuitem header__menuitem_flex-item">
+                    <span class="subsections">
+                        <button class="subsection-btn">НАРУШЕНИЯ РЕЧИ</button>
+                        <div class="menu">
+                            <div class="block-for-square">
+                                <div class="square"></div>
+                            </div>
+                            <menu class="menu-subsections">
+                                <menuitem><a href="#">Запуск речи</a></menuitem>
+                                <menuitem><a href="#">Коррекция звукопроизношения</a></menuitem>
+                                <menuitem><a href="#">Логопедический массаж</a></menuitem>
+                                <menuitem><a href="#">Stop заикание</a></menuitem>
+                            </menu>
+                        </div>
+                    </span>
+                </menuitem>
+                <menuitem class="header__menuitem header__menuitem_flex-item">
+                    <span class="subsections">
+                        <button class="subsection-btn">ЗАДЕРЖКА И НАРУШЕНИЕ РАЗВИТИЯ</button>
+                        <div class="menu">
+                            <div class="block-for-square">
+                                <div class="square"></div>
+                            </div>
+                            <menu class="menu-subsections">
+                                <menuitem><a href="#">Запуск речи</a></menuitem>
+                                <menuitem><a href="#">Коррекция звукопроизношения</a></menuitem>
+                                <menuitem><a href="#">Логопедический массаж</a></menuitem>
+                                <menuitem><a href="#">Stop заикание</a></menuitem>
+                            </menu>
+                        </div>
+                    </span>
+                </menuitem>
+                <menuitem class="header__menuitem header__menuitem_flex-item">
+                    <span class="subsections">
+                        <button class="subsection-btn">ПРОБЛЕМЫ ОБУЧЕНИЯ И ПОВЕДЕНИЯ</button>
+                        <div class="menu">
+                            <div class="block-for-square">
+                                <div class="square"></div>
+                            </div>
+                            <menu class="menu-subsections">
+                                <menuitem><a href="#">Запуск речи</a></menuitem>
+                                <menuitem><a href="#">Коррекция звукопроизношения</a></menuitem>
+                                <menuitem><a href="#">Логопедический массаж</a></menuitem>
+                                <menuitem><a href="#">Stop заикание</a></menuitem>
+                            </menu>
+                        </div>
+                    </span>
+                </menuitem>
+                <menuitem class="header__menuitem header__menuitem_flex-item">
+                    <span class="subsections">
+                        <button class="subsection-btn">НАША КОМАНДА</button>
+                        <div class="menu">
+                            <div class="block-for-square">
+                                <div class="square"></div>
+                            </div>
+                            <menu class="menu-subsections">
+                                <menuitem><a href="#">Запуск речи</a></menuitem>
+                                <menuitem><a href="#">Коррекция звукопроизношения</a></menuitem>
+                                <menuitem><a href="#">Логопедический массаж</a></menuitem>
+                                <menuitem><a href="#">Stop заикание</a></menuitem>
+                            </menu>
+                        </div>
+                    </span>
+                </menuitem>
             </menu>
         </div>
     </header>
+
     <section class="slider slider_height">
         <!-- Slider main container -->
         <div class="swiper">
@@ -31,43 +92,51 @@
                 <div class="swiper-slide">
                     <div class="swiper_background"></div>
                     <img src="img/neyrokorrekciya.jpg" alt="neyrokorrekciya">
-                    <div class="swiper_text_1">
-                        <span>ЗАПУСК РЕЧИ У НЕГОВОРЯЩИХ ДЕТЕЙ ОТ НУЛЯ ДО ФРАЗОВОЙ РЕЧИ</span>
-                    </div>
-                    <div class="swiper_text_2">
-                        <span>Как понять, что пора обращаться к специалисту?</span>
-                    </div>
-                    <div class="swiper_more">
-                        <a href="#">Узнать</a>
+                    <div class="swiper__text-block">
+                        <div class="swiper_text_1">
+                            <span>ЗАПУСК РЕЧИ У НЕГОВОРЯЩИХ ДЕТЕЙ ОТ НУЛЯ ДО ФРАЗОВОЙ РЕЧИ</span>
+                        </div>
+                        <div class="swiper_text_2">
+                            <span>Как понять, что пора обращаться к специалисту?</span>
+                        </div>
+                        <div class="swiper_more">
+                            <a href="#">Узнать</a>
+                        </div>
                     </div>
                 </div>
+
                 <div class="swiper-slide">
                     <div class="swiper_background"></div>
                     <img src="img/photo.jpg" alt="photo">
-                    <div class="swiper_text_1">
-                        <span>КУРС "Заикание STOP"</span>
-                    </div>
-                    <div class="swiper_text_2">
-                        <span>
-                            Ваш ребёнок заикается?<br>
-                            Ему сложно выразить свои мысли и общаться со сверстниками?
-                        </span>
-                    </div>
-                    <div class="swiper_more">
-                        <a href="#">Узнать</a>
+                    <div class="swiper__text-block">
+                        <div class="swiper_text_1">
+                            <span>КУРС "Заикание STOP"</span>
+                        </div>
+                        <div class="swiper_text_2">
+                            <span>
+                                Ваш ребёнок заикается?<br>
+                                Ему сложно выразить свои мысли и общаться со сверстниками?
+                            </span>
+                        </div>
+                        <div class="swiper_more">
+                            <a href="#">Подробнее</a>
+                        </div>
                     </div>
                 </div>
+
                 <div class="swiper-slide">
                     <div class="swiper_background"></div>
                     <img src="img/photo_2022-10-20_15-.jpg" alt="photo">
-                    <div class="swiper_text_1">
-                        <span>LAKELAND ROUTES</span>
-                    </div>
-                    <div class="swiper_text_2">
-                        <span>Explore Helsinki and the Finnish lake on an unforgettable road trip alone or with your loved ones.</span>
-                    </div>
-                    <div class="swiper_more">
-                        <a href="#">Узнать</a>
+                    <div class="swiper__text-block">
+                        <div class="swiper_text_1">
+                            <span>LAKELAND ROUTES</span>
+                        </div>
+                        <div class="swiper_text_2">
+                            <span>Explore Helsinki and the Finnish lake on an unforgettable road trip alone or with your loved ones.</span>
+                        </div>
+                        <div class="swiper_more">
+                            <a href="#">Read more</a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -82,8 +151,12 @@
             <div class="swiper-scrollbar"></div>
         </div>
     </section>
+
     <footer class="footer">
-        <div></div>
+        <div class="footer__item"><a href="#">Часто задаваемые вопросы</a></div>
+        <div class="footer__item"><a href="#">Отзывы</a></div>
+        <div class="footer__item"><a href="#">Как нас найти?</a></div>
+        <div class="footer__item"><a href="#">Контакты</a></div>
     </footer>
     
     <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
--- js/script.js
+++ js/script.js
@@ -21,3 +21,4 @@ const swiper = new Swiper('.swiper', {
     //   delay: 5000,
     // },
   });
+
